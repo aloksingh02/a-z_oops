e0f3790ec12f6e084636c28faf2527322b82972b
Asus <singhalokkumar427@gmail.com>
2025-07-06 22:25:11 +0530

    kadane new method

--- src/method_basic2.java
+++ src/method_basic2.java
@@ -22,19 +22,13 @@ public class method_basic2 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // Input array size
         System.out.print("Enter number of elements: ");
         int n = sc.nextInt();
-
-        // Input array elements
         int[] arr = new int[n];
         System.out.println("Enter array elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-
-        // Call Kadane method
         int result = kadane(arr);
         System.out.println("Maximum Subarray Sum: " + result);
     }
